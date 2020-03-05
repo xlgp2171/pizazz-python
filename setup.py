b3da29cdf5380ec58a69d804360a2c6d17bab16b
xlgp2 <xlgp2@pizazz2>
2020-03-05 21:51:42 +0800

    基础代码

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(
-    name='pizazz',
+	name='pizazz',
     version='1.0.0',
     description='pizazz-python',
     license='MIT',
@@ -12,7 +12,7 @@ setup(
     platforms='any',
     include_package_data=True,
     packages=[
-        'piz_base',
+    	'piz_base',
         'piz_base.common',
         'piz_base.tool',
         'piz_component',
@@ -21,15 +21,18 @@ setup(
         'piz_component.kafka.consumer',
         'piz_component.kafka.producer',
         'piz_component.redis'
-    ], package_data={
+	],
+    package_data={
         'piz_component.kafka.resource': [
-            'piz_component/kafka/resource/*.yml'
-        ]
-    }, install_requires=[
+         	'piz_component/kafka/resource/*.yml'
+		]
+	},
+    install_requires=[
         'kafka-python>=1.4.4',
-        'redis=3.2.0',
+        'redis>=3.2.0',
         'uri>=2.0.1',
         'pyyaml>=5.1',
         'python-dateutil>=2.8.0',
-        'redis_py_cluster>=2.0.0'
-    ], zip_safe=False)
+        'redis-py-cluster>=2.1.0'
+	],
+    zip_safe=False)
